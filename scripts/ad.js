4488591ae9179e8ab4ea35337543b3f6f42f2633
Fubiao Ouyang <ouyfb@weichai.com>
2025-06-11 10:19:56 +0900

    google ad

--- scripts/ad.js
+++
@@ -1,4 +0,0 @@
-hexo.extend.filter.register('theme_inject', function(injects) {
-  //名字路径等都可以随意修改，为了方便下文都以这里的定义为主
-  injects.bodyEnd.raw('google-ad-js', '<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9670282672953561" crossorigin="anonymous"></script>', {}, {cache: true});
-});
